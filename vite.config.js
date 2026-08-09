759c055739aced6964015a66eaeae2302831fa09
ChiriFran <franchiri00@gmail.com>
2026-08-09 14:38:49 -0300

    fix: change base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // <<--- Esta línea es clave para rutas relativas en producción
+  base: '/',
   plugins: [react()],
 })
